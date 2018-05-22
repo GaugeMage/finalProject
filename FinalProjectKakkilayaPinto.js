10f3aa60c1fff5fb10e4d1e1c394b8ed970c5ee7
GaugeMage <rkakkilaya@gmail.com>
2018-05-21 23:05:53 -0400

    Update FinalProjectKakkilayaPinto.js

--- FinalProjectKakkilayaPinto.js
+++ FinalProjectKakkilayaPinto.js
@@ -3,3 +3,36 @@ Programmers: Rohan Kakkilaya, & James Pinto
 Date Created: 5-21-18
 finalProject
 */
+
+//Variables
+var cardNumber;
+var cardType;
+var cardPicker;
+var playerCardShown;
+var playerCardHidden;
+var compCardShown;
+var compCardHidden;
+
+initialize();
+
+//Picks a random card
+cardNumber = Math.floor(Math.random() * 11) + 2;
+cardType = Math.floor(Math.random() * 4) + 1;
+switch (cardType){
+  case 1:
+    cardType = "C";
+    break;
+  case 2:
+    cardType = "D";
+    break;
+  case 3:
+    cardType = "H";
+    break;
+  case 4:
+    cardType = "S";
+    break;
+}
+
+//Function for inititializing when the game starts
+function initialize(){
+}
