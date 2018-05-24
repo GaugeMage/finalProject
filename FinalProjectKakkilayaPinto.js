92513b92d2a4462e03d4d633470bc3dff805439e
pintojames5 <39493987+pintojames5@users.noreply.github.com>
2018-05-23 23:34:08 -0400

    Update FinalProjectKakkilayaPinto.js

--- FinalProjectKakkilayaPinto.js
+++ FinalProjectKakkilayaPinto.js
@@ -14,6 +14,7 @@ var playerCardShown;
 var playerCardHidden;
 var compCardShown;
 var compCardHidden;
+var cardCount = [4,4,4,4,4,4,4,4,4,4,4,4,4]; //Card Value: Ace,2,3,4,5,6,7,8,9,10,Jack,Queen,King
 
 initialize();
 
@@ -84,10 +85,10 @@ console.log(card);
 
 //When the player presses the button to start the game
 function startGame(){
-  alert("Here are the rules of Blackjack:");
-  gameStart = prompt("If you don't wanna play now type \'exit\'. If you wanna play Blackjack, type in your name.");
+  alert(rules());
+  //gameStart = prompt("If you don't wanna play now type \'exit\'. If you wanna play Blackjack, type in your name.");
   if (gameStart == "exit"){
-    //Startgame
+    throw new Error('This is not an error. This is just to abort game');
   } else {
     //Start game
     document.getElementById("rules").style.visibility = "visible";
@@ -100,7 +101,7 @@ function startGame(){
 
 //When the player clicks the rules button
 function rules(){
-  alert("Here are the rules of Blackjack:");
+  alert("Here are the rules of Blackjack: \n-Ace & Jack, King, Queen is blackjack and an automatic win \n-Try to get as close to 21 by hitting without going over or you bust \n-Stand when you are satisfied with your hand \n-The computers hand is played after the player stands \n-1 point is given per round for the player closest to 21 \n-Press restart to restart the game \n-Press rules to show rules");   
 }
 
 //When the player clicks the hit button
@@ -110,14 +111,19 @@ function hit(){
 
 //When the player clicks the stand button
 function stand(){
+    document.getElementById("hit").style.visibility = "hidden";
+    document.getElementById("stand").style.visibility = "hidden";
+}
 
+function cpuTurn{
+    stand();
 }
 
 //When the player clicks the restart button
 function restart(){
   userRestart = prompt("Are you sure you want to restart your game?");
   if (userRestart == "Yes" || userRestart == "yes"){
-
+      initialize();
   } else if (userRestart == "No" || userRestart == "no"){
 
   }
@@ -125,4 +131,12 @@ function restart(){
 
 //Function for inititializing when the game starts
 function initialize(){
+    document.getElementById("rules").style.visibility = "hidden";
+    document.getElementById("startGame").style.visibility = "visible";
+    document.getElementById("hit").style.visibility = "hidden";
+    document.getElementById("stand").style.visibility = "hidden";
+    document.getElementById("restart").style.visibility = "hidden";
+    var cardCount = [4,4,4,4,4,4,4,4,4,4,4,4,4];
 }
+
+
